faf6c0645b93b1e680371d18e9fa6e5302c8b57a
ChrisToffee <chris.s.dennis@gmail.com>
2014-09-21 15:08:44 +0100

    Initial commit

---
+++ src/game/colours.js
@@ -0,0 +1,22 @@
+game.module(
+    'game.colours'
+)
+.body(function() {
+game.crowd = [
+
+    { name: 'rock',
+        1: { 'name': 'mosher-green', 'colorCode': 1, 'fill': '662d91' },
+        2: { 'name': 'mosher-yellow', 'colorCode': 2, 'fill': 'F1B65A' },
+        3: { 'name': 'mosher-blue', 'colorCode': 3, 'fill': '243C4A'  },
+        4: { 'name': 'mosher-red', 'colorCode': 4, 'fill': '4F2C39' },
+        5: { 'name': 'mosher-grey', 'colorCode': 5, 'fill': '0A1C1F' }
+    },
+    { name: 'pop',
+        1: { 'colour': 'GREEN', 'colorCode': 1, 'fill': '3A5E48' },
+        2: { 'colour': 'YELLOW', 'colorCode': 2, 'fill': 'FFFF00' },
+        3: { 'colour': 'BLUE', 'colorCode': 3, 'fill': '0071BC'  },
+        4: { 'colour': 'RED', 'colorCode': 4, 'fill': 'FF003C' },
+        5: { 'colour': 'RED', 'colorCode': 5, 'fill': 'FF003C' }
+    }
+];
+});
